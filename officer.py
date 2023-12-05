1711858f0ff0fd1530ca7a4f1796cfc77d6e8ce1
Isabella Schmidt <152990358+atavism-kipper@users.noreply.github.com>
2023-12-05 21:16:44 +0700

    Create officer.py

---
+++ officer.py
@@ -0,0 +1,12 @@
+# officer.py
+class Officer:
+    def __init__(self):
+        self.officer_list = []
+
+    def add_officer(self, name, badge_number):
+        # Add officer logic here
+        pass
+
+    def list_officers(self):
+        # List officers logic here
+        pass
