564803bcf0afb51710d4da7a94d8b453e35187e7
Isabella Schmidt <152990358+atavism-kipper@users.noreply.github.com>
2023-12-05 21:17:08 +0700

    Create case_manager.py

---
+++ case_manager.py
@@ -0,0 +1,12 @@
+# case_manager.py
+class CaseManager:
+    def __init__(self):
+        self.case_list = []
+
+    def create_case(self, description, status):
+        # Create case logic here
+        pass
+
+    def list_cases(self):
+        # List cases logic here
+        pass
